f5acc15c5ca93c925ac16de1c4f7d7a9abc2cb33
joaopedrosilva <jpesmail@gmail.com>
2018-02-06 16:13:45 +0000

    a

--- StudioProjects/CustomViewWithCanvas/app/src/main/java/com/example/joaopedrosilva/customviewwithcanvas/Graph.kt
+++ StudioProjects/CustomViewWithCanvas/app/src/main/java/com/example/joaopedrosilva/customviewwithcanvas/Graph.kt
@@ -9,17 +9,14 @@ import android.view.View
 
 @Suppress("PrivatePropertyName", "LocalVariableName", "JoinDeclarationAndAssignment")
 class Graph : View {
-    private var barCenterVertical: Float = 0f
     private var barCenterHorizontal: Float = 0f
-    private var top: Float = 0f
-    private var bottom: Float = 0f
-    private var left: Float = 0f
-    private var right: Float = 0f
-    private var sizeBar: Float = 0f
+    private var barHeight: Float = 0f
+    private var textSizeMessage: Int = 0
     private var oldSize: Float = 0f
     private var cubeSize: Float = 0f
-    private var barHeight: Int = 0
     private var barWidth: Int = 0
+    private var margin: Int = 0
+    private var sliderRect: Rect = Rect()
     private val listColor = listOf(
         context.resources.getColor(android.R.color.holo_red_light),
         context.resources.getColor(android.R.color.holo_orange_dark),
@@ -27,8 +24,7 @@ class Graph : View {
         context.resources.getColor(android.R.color.holo_green_light),
         context.resources.getColor(android.R.color.holo_blue_light)
     )
-    private val cube =
-        listOf(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100)
+
 
     data class Points(val name: String, val position: Float)
 
@@ -45,22 +41,31 @@ class Graph : View {
     private val pointPaintText: Paint
     private val pointPaintBg: Paint
     private val labelPaint: Paint
-    private val paint= Paint()
+    private val paint = Paint()
+    private var intervals: FloatArray = floatArrayOf()
     private var arrowPoint: Path = Path()
     private var rectanglePoint = Rect()
     private var rectf = RectF()
+    private var sliderWidth: Int = 200
 
     @SuppressLint("NewApi")
     @Suppress("ConvertSecondaryConstructorToPrimary")
     @JvmOverloads
-    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0)
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0
+    )
             : super(context, attrs, defStyleAttr, defStyleRes) {
         isSaveEnabled = true
-
+        this.intervals = floatArrayOf(0.2f, 0.4f, 0.6f, 0.8f, 1f)
         //read xml attributes
         val ta = context.theme.obtainStyledAttributes(attrs, R.styleable.GraphBar, 0, 0)
         barWidth = ta.getDimensionPixelSize(R.styleable.GraphBar_barWidth, 0)
-        barHeight = ta.getDimensionPixelSize(R.styleable.GraphBar_barHeight, 0)
+        margin = ta.getDimensionPixelSize(R.styleable.GraphBar_margin, 0)
+        margin += 10
+        textSizeMessage = ta.getDimensionPixelSize(R.styleable.GraphBar_textSizeMessage, 20)
         pointPaintText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             textSize = 20f
             color = ContextCompat.getColor(context, android.R.color.white)
@@ -78,103 +83,107 @@ class Graph : View {
             typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
         }
 
+
+
         ta.recycle()
 
 
-
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(barWidth, barHeight)
-    }
 
     override fun onDraw(canvas: Canvas) {
-        drawBar(canvas)
-    }
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        if (h > 0) {
-            barCenterVertical = ((height - paddingTop - paddingBottom) / 2).toFloat()
-        }
-        if (w > 0) {
-            barCenterHorizontal = ((width - paddingLeft - paddingRight) / 2).toFloat()
-        }
-
-
-    }
-
-    private fun drawBar(canvas: Canvas) {
-        if(height < 0 || width < 0){
+        if (height < 0 || width < 0) {
             return
         }
-        top = barCenterVertical - barHeight
-        bottom = barCenterVertical + barHeight
-        left = barCenterHorizontal - barWidth
-        right = barCenterHorizontal + barWidth
-        sizeBar = bottom - top
-        cubeSize = sizeBar / cube.size
+        barHeight = height.toFloat() - (margin * 2) - paddingBottom - paddingTop
+        drawBarColor(canvas)
+        drawMessages(canvas)
+    }
 
-        for (x in cube) {
-            paint.reset()
-            paint.color = getColor(x)
-            rectf = RectF(left, bottom - oldSize - cubeSize, right, bottom - oldSize)
-            canvas.drawRect(rectf, paint)
-            oldSize += cubeSize
-        }
-        if (listPoints != null) {
-            for (points in listPoints) {
-                val bounds = Rect()
-                pointPaintText.getTextBounds(points.name, 0, points.name.length, bounds)
-                val widthMin = if (bounds.width() < 32) 32 else bounds.width()
-                val heightMin = if (bounds.height() < 16) 16 else bounds.height()
-                val y = ((bottom + paddingBottom) + (heightMin / 2)) - (sizeBar * points.position)
-                val x = (barCenterHorizontal - barWidth) - widthMin - 30
-                arrowPoint.reset()
-                arrowPoint.apply {
-                    moveTo(20f, 0f)
-                    lineTo(0f, 10f)
-                    lineTo(0f, -10f)
-                    close()
-                    offset(x + 35, y - 6.5f)
-                }
-                //design rect
-                rectanglePoint = Rect(
-                    x.toInt() - 4,
-                    (y.toInt()) - heightMin,
-                    (x.toInt() + 4) + widthMin,
-                    y.toInt() + 4
-                )
-                canvas.drawPath(arrowPoint, pointPaintBg)
-                canvas.drawRect(rectanglePoint, pointPaintBg)
-                canvas.drawText(points.name, x, y, pointPaintText)
-            }
-        }
 
+    private fun drawPoints(canvas: Canvas) {
+
+
+//        if (listPoints != null) {
+//            for (points in listPoints) {
+//                val bounds = Rect()
+//                pointPaintText.getTextBounds(points.name, 0, points.name.length, bounds)
+//                val widthMin = if (bounds.width() < 32) 32 else bounds.width()
+//                val heightMin = if (bounds.height() < 16) 16 else bounds.height()
+//                val y = ((bottom + paddingBottom) + (heightMin / 2)) - (sizeBar * points.position)
+//                val x = (barCenterHorizontal - barWidth) - widthMin - 30
+//                arrowPoint.reset()
+//                arrowPoint.apply {
+//                    moveTo(20f, 0f)
+//                    lineTo(0f, 10f)
+//                    lineTo(0f, -10f)
+//                    close()
+//                    offset(x + 35, y - 6.5f)
+//                }
+//                //design rect
+//                rectanglePoint = Rect(
+//                    x.toInt() - 4,
+//                    (y.toInt()) - heightMin,
+//                    (x.toInt() + 4) + widthMin,
+//                    y.toInt() + 4
+//                )
+//                canvas.drawPath(arrowPoint, pointPaintBg)
+//                canvas.drawRect(rectanglePoint, pointPaintBg)
+//                canvas.drawText(points.name, x, y, pointPaintText)
+//            }
+//        }
+//
+
+
+    }
+
+    private fun drawMessages(canvas: Canvas) {
         for (label in listLabels) {
-            labelPaint.getTextBounds(label.name, 0, label.name.length, rectanglePoint)
+            paint.reset()
+            paint.apply {
+                textSize = textSizeMessage.toFloat()
+                isAntiAlias=true
+                color = ContextCompat.getColor(context, android.R.color.darker_gray)
+                textAlign = Paint.Align.LEFT
+                typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+            }
+            val bounds= Rect()
+            paint.getTextBounds(label.name, 0, label.name.length, bounds)
 
-            val y = (bottom + paddingBottom) - (sizeBar * label.position)
-            val x = (barCenterHorizontal + barWidth) + 30
 
-            canvas.drawText(label.name, x, y, labelPaint)
+            val y = margin + (barHeight * label.position)
+            val x = ((width /2) + barWidth  + 30).toFloat()
+
+            canvas.drawText(label.name, x, y, paint)
+        }
+    }
+
+    private fun drawBarColor(canvas: Canvas) {
+
+        val startEndOffset = (width + margin - barWidth) / 2
+        var y0 = margin.toFloat()
+        val x1 = (width - startEndOffset).toFloat()
+
+        val x0 = (0 + startEndOffset).toFloat()
+
+        paint.apply {
+            reset()
+            isAntiAlias = true
         }
 
+        listColor.forEachIndexed { index, color ->
+            val y1 =
+                (intervals[index] * barHeight) + margin
 
-    }
-
-    private fun getColor(x:Int):Int{
-          return when (x) {
-                in 0..10 -> listColor[4]
-                in 11..18 -> listColor[3]
-                in 19..46 -> listColor[2]
-                in 47..76 -> listColor[1]
-                in 77..100 -> listColor[0]
-                else -> listColor[0]
+            paint.apply {
+                this.color = color
             }
 
-
+            canvas.drawRect(x0, y0, x1, y1, paint)
+            y0 = y1
+        }
     }
+
     fun setPoints(list: ArrayList<Points>) {
         this.listPoints = list
         invalidate()
--- StudioProjects/CustomViewWithCanvas/app/src/main/java/com/example/joaopedrosilva/customviewwithcanvas/MainActivity.kt
+++ StudioProjects/CustomViewWithCanvas/app/src/main/java/com/example/joaopedrosilva/customviewwithcanvas/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.joaopedrosilva.customviewwithcanvas
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var graph :Graph
+    private lateinit var graph: Graph
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    graph = findViewById (R.id.graph)
-setPoint()
+        graph = findViewById(R.id.graph)
+        setPoint()
     }
-fun setPoint(){
-    Handler().postDelayed({
-        //doSomethingHere()
-    }, 10000)
-    graph.setPoints(arrayListOf(Graph.Points("160", 1f),Graph.Points("60", 0.5f),Graph.Points("160", 0.2f) ))
-}
+
+    private fun setPoint() {
+        graph.setPoints(
+            arrayListOf(
+                Graph.Points("160", 1f),
+                Graph.Points("60", 0.5f),
+                Graph.Points("160", 0.2f)
+            )
+        )
+    }
 
 }
